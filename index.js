8bb55520ab9fa43b8f14a01975a2b58e0c4d678f
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-04-14 21:48:51 -0400

    Created the first prompt

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const inquirer = require('inquirer')
 
-inquirer.createPromptModule([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'firstCheck',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
-    }
-])
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'firstCheck',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ])
+    .then((data) => {
+        if (data === 'View all departments') {
+            data.query('SELECT Departments FROM ', (err, results)=>{
+                err ? console.error(err) : console.log(results)
+            })
+        }
+    })
