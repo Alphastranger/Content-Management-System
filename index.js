b5db9580c935e4c4f7c3774640d1e67b38a3790c
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-04-14 23:29:48 -0400

    Made a serverjs file, but probably going to delete it.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const inquirer = require('inquirer')
+const mysql = require('mysql2')
+const cTable = require('console.table')
 
-inquirer
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: 'library_db'
+    },
+    console.log(`Connected to the library database.`)
+)
+const checklist = function (){
+    inquirer
     .prompt([
         {
             type: 'list',
             message: 'What would you like to do?',
             name: 'firstCheck',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
         }
     ])
     .then((data) => {
         if (data === 'View all departments') {
-            data.query('SELECT Departments FROM ', (err, results)=>{
+            db.query('SELECT * FROM department', (err, results)=>{
                 err ? console.error(err) : console.log(results)
             })
+        } else if (data === 'View all roles'){
+            db.query('SELECT * FROM roles'), (err, results) => {
+                err ? console.error(err) : console.log(results)
+            }
+        } else if (data === 'View all employees'){
+            db.query('SELECT * FROM employee'), (err, results) => {
+                err ? console.error(err) : console.log(results)
+            }
+        } else if (data === 'Add a department'){
+            db.query('SELECT')
         }
     })
+}
+module.exports = checklist
